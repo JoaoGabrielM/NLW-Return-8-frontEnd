5a5f7390dbbfcca4d2492c4fbe723f309bd82afb
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-02 19:18:15 -0300

    Fazendo um texto aparecer ao apertar o botão

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,9 +1,17 @@
 import { ChatTeardropDots } from 'phosphor-react'
+import { useState } from 'react'
 
 export function Widget() {
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+    function toggleWidgetVisibility() {
+        setIsWidgetOpen(!isWidgetOpen); // Invertendo o valor de isWidgetOpen quando houver um onClick no button
+    }
+
     return (
         <div className="absolute bottom-5 right-5">
-            <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"> {/* colocando a cor definida nas configurações do tailwind*/}
+            { isWidgetOpen && <p>Hello World</p> }
+            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"> {/* colocando a cor definida nas configurações do tailwind*/}
                 <ChatTeardropDots className="w-6 h-6"/> {/* A tag ground no button diz que todos os elementos dentro dele são do mesmo grupo */}
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"> {/* O ground-hover ativa quando houver um hover no grupo do button */}
