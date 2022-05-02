3446115b9e627fcf6e36f83cc9ca64fdac1332ea
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-02 18:26:42 -0300

    Criando propriedades em elementos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,19 @@
+interface ButtonProps {
+    text?: String; // Colocando a propriedade como opcional e string
+}
+
+function Button(props: ButtonProps) {
+    return <button>{props.text ?? "Default"}</button> // Recebendo uma string, ou caso não seja enviado, coloca "Default"
+}
+
 function App() {
-    return <h1>Hello World</h1>
+    return (
+        <div>
+            <Button text="Enviar"></Button>
+            <Button text="OK"></Button>
+            <Button/>
+        </div>
+    )
 }
 
 export default App
