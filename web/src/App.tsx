349a7e201d5e53d8019f8ca6a684317446d2adbe
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-02 18:53:45 -0300

    Rearanjando os arquivos e instalando o phosphor-react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-    text?: String; // Colocando a propriedade como opcional e string
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
-    return <button className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? "Default"}</button> // Recebendo uma string, ou caso não seja enviado, coloca "Default"
+export function App() {
+    return <Widget/>
 }
-
-function App() {
-    return (
-        <div className="flex gap-2">
-            <Button text="Enviar"></Button>
-            <Button text="OK"></Button>
-            <Button/>
-        </div>
-    )
-}
-
-export default App
