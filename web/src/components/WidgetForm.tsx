e3526e3e7486e25948dbcfadb757564cff07a43b
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-03 18:18:45 -0300

    Criando o visual dos botões dos tipos de feedback

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,18 +1,30 @@
 import { CloseButton } from "./CloseButton";
 
-const feedbackTypes = {
+import bugImageURL from '../assets/bug.svg';
+import ideaImageURL from '../assets/idea.svg';
+import thoughtImageURL from '../assets/thought.svg';
+
+const feedbackTypes = { // Foi criado dessa forma para facilitar a manutenção, pois basta adicionar um novo elemento que o visual será alterado
     BUG: {
         title: "Problema",
         image: {
-            source: "",
-            alt: ""
+            source: bugImageURL,
+            alt: "Imagem de um inseto"
         }
     },
     IDEA: {
         title: "Ideia",
+        image: {
+            source: ideaImageURL,
+            alt: "Imagem de uma lâmpada"
+        }
     },
     OTHER: {
         title: "Outro",
+        image: {
+            source: thoughtImageURL,
+            alt: "Imagem de um balão de pensamento"
+        }
     }
 }
 
@@ -26,11 +38,24 @@ export function WidgetForm() {
             </header>
 
             <div className="flex py-8 gap-2 w-full">
-                <button></button>
+                { Object.entries(feedbackTypes).map(([key, value]) => { // Está quebrando o array retornado pelo map nos 2 valores do retorno, pra evitar usar item[0] e item[1] para acessar os valores
+                    return (
+                        <button
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            // O focus:outline-none faz com que suma a borda padrão caso o botão seja selecionado pelo teclado usando o TAB
+                            //onClick={}
+                            type="button"
+                        >
+                            <img src={ value.image.source } alt={ value.image.alt }/>
+                            <span>{ value.title }</span>
+                        </button>
+                    );
+                }) } {/* Vai percorrer o objeto criado no começo da página */}
             </div>
 
             <footer className="text-xs text-neutral-400">
-                Feito com ♥ por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/jo%C3%A3o-gabriel-machado-978565120/">Threaxe</a>
+                Feito com ♥ por <a className="underline underline-offset-2" target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-gabriel-machado-978565120/">Threaxe</a>
             </footer>
         </div>
     );
