104942c1978cbad4c856230c3ae9879fdb8b485f
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-03 18:30:12 -0300

    Adicionando a mudança de estado das opções de botões

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,4 +1,5 @@
 import { CloseButton } from "./CloseButton";
+import { useState } from "react";
 
 import bugImageURL from '../assets/bug.svg';
 import ideaImageURL from '../assets/idea.svg';
@@ -28,7 +29,11 @@ const feedbackTypes = { // Foi criado dessa forma para facilitar a manutenção,
     }
 }
 
+type FeedbackType = keyof typeof feedbackTypes; // Torna os tipos de feedback aceitáveis somente os que são chave de feedbackTypes
+
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null); // Torna os tipos aceitos somente os FeedbackType ou null
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {/* Na classe acima, o tamanho do elemento é ajustado para ser diferente no mobile ou dispositivos maiores, do md pra cima fica auto, no sm ocupa a largura toda da tela */}
@@ -37,22 +42,27 @@ export function WidgetForm() {
                 <CloseButton/>
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => { // Está quebrando o array retornado pelo map nos 2 valores do retorno, pra evitar usar item[0] e item[1] para acessar os valores
-                    return (
-                        <button
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            // O focus:outline-none faz com que suma a borda padrão caso o botão seja selecionado pelo teclado usando o TAB
-                            //onClick={}
-                            type="button"
-                        >
-                            <img src={ value.image.source } alt={ value.image.alt }/>
-                            <span>{ value.title }</span>
-                        </button>
-                    );
-                }) } {/* Vai percorrer o objeto criado no começo da página */}
-            </div>
+            {/* Quando não tiver um tipo de feedback, ele retorna os botões. Caso tenha um tipo, ele age diferente */}
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
+                    { Object.entries(feedbackTypes).map(([key, value]) => { // Está quebrando o array retornado pelo map nos 2 valores do retorno, pra evitar usar item[0] e item[1] para acessar os valores
+                        return (
+                            <button
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                                // O focus:outline-none faz com que suma a borda padrão caso o botão seja selecionado pelo teclado usando o TAB
+                                onClick={() => setFeedbackType(key as FeedbackType)}
+                                type="button"
+                            >
+                                <img src={ value.image.source } alt={ value.image.alt }/>
+                                <span>{ value.title }</span>
+                            </button>
+                        );
+                    }) } {/* Vai percorrer o objeto criado no começo da página */}
+                </div>
+            ) : (
+                <p>Hello World</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" target="_blank" href="https://www.linkedin.com/in/jo%C3%A3o-gabriel-machado-978565120/">Threaxe</a>
