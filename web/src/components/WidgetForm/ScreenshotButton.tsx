110d3391f5933d5a3394ee5ad51d52309deeb2ab
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-03 21:16:39 -0300

    Criando o botão de apagar a screenshot e mostrando ela

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,9 +1,14 @@
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import html2canvas from "html2canvas";
 import { useState } from "react";
 import { Loading } from "../Loading";
 
-export function ScreenshotButton() {
+interface ScreenshotButtonProps {
+    screenshot: string | null;
+    onScreenshotTook: (screenshot: string | null) => void;
+}
+
+export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
     async function handleTakeScreenshot() {
@@ -12,16 +17,33 @@ export function ScreenshotButton() {
         const canvas = await html2canvas(document.querySelector("html")!);
         const base64image = canvas.toDataURL('image/png');
 
+        onScreenshotTook(base64image); // Enviando a foto para o componente pai
+
         setIsTakingScreenshot(false);
     }
-
+    if (screenshot){
+        return(
+            <button
+                type="button"
+                className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                onClick={() => onScreenshotTook(null)}
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    backgroundPosition: "right bottom",
+                    backgroundSize: 180
+                }}
+            >
+                <Trash weight="fill"/>
+            </button>
+        )
+    }
     return (
         <button
             type="button"
             onClick={handleTakeScreenshot}
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
-            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100"/> }
+            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-100"/> /* Enquanto não tirar o print, carrega o loading */}
         </button>
     );
 }
