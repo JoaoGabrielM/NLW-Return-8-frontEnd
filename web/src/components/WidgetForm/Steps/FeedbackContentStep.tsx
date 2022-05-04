60f0e7a9346bb8068cdc4535fd21b6c18aee1285
João Gabriel Machado <joaogabriel013@gmail.com>
2022-05-03 21:31:24 -0300

    Finalizando o formulário

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -11,8 +11,14 @@ interface FeedBackContentStepProps {
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedBackContentStepProps) {
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState("");
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({ screenshot, comment });
+    }
 
     return (
         <>
@@ -28,10 +34,11 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton/>
             </header>
 
-            <form className="my-4 w-full">
+            <form onSubmit={ handleSubmitFeedback } className="my-4 w-full">
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={ event => setComment(event.target.value) }
                 />
 
                 <footer className="flex gap-2 mt-2">
@@ -41,7 +48,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                     />
                     <button 
                         type="submit"
-                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                        disabled={comment.length == 0}
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >   {/* 
                             focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500
                             Distancia a borda ao focar, coloca o espaço vazio como sendo o mesmo do background da página e muda a cor da borda no foco
